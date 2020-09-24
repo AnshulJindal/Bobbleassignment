d9497690cb5f5103dc8d1277a1039e7fba87f996
AnshulJindal <ajindal700@gmail.com>
2020-09-24 22:35:08 +0530

    bobble assignment

--- src/App.js
+++ src/App.js
@@ -1,42 +1,68 @@
-import React from 'react';
-import GoogleLogin from 'react-google-login';
+import React from "react";
+import GoogleLogin from "react-google-login";
 import FacebookLogin from "react-facebook-login";
-import './App.css'
-import { Form, Button, Row, Col, Container} from "reactstrap";
-class SignupForm extends React.Component{
-  handleSubmit=(values)=>
-  {
-    console.log(values);
-  }
-  render()
-  {
-    const responseFacebook=(response)=>{
+import "./App.css";
+import axios from "axios";
+import { Form, Button, Row, Col, Container } from "reactstrap";
+class SignupForm extends React.Component {
+  state = { email: "", password: "" };
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("https://reqres.in/api/register", {
+        email: this.state.email,
+        password: this.state.password,
+      });
       console.log(response);
-      alert("Signup Succesfully with"+response.profileObj.email+"account and check console for other details");
+      alert("Signup Successful");
+    } catch (error) {
+      alert("Missing password");
     }
-    const responseGoogle = (response)=>{
-      console.log(response.profileObj);
-       alert("Signup Succesfully with"+response.profileObj.email+"account and check console for other details");
-     };
-    return(
+  };
+  handleEmailChange = (e) => {
+    this.setState({ email: e.currentTarget.value });
+  };
+  handlePasswordChange = (e) => {
+    this.setState({ password: e.currentTarget.value });
+  };
+  responseFacebook = (response) => {
+    console.log(response);
+    alert(
+      "Signup Succesfully with" +
+        response.id +
+        "account and check console for other details"
+    );
+  };
+  responseGoogle = (response) => {
+    console.log(response.profileObj);
+    alert(
+      "Signup Succesfully with" +
+        response.profileObj.email +
+        "account and check console for other details"
+    );
+  };
+  render() {
+    return (
       <Container className="container">
         <Row>
-          <Col sm="10" className="x">
-            <img src='logo.png'/>
+          <Col sm="5" className="x">
+            <img src="logo.png" />
           </Col>
         </Row>
         <Row>
           <Col sm="5" className="x">
             <h3>SIGN UP</h3>
-            <br/>
-            <h1 style={{ fontWeight:"normal" }}>Create your account</h1>
-            <h5 style={{ fontWeight:"normal" }}>sdhla kds'a kd'JF skan kldfakn aklhfaehv aklhfah kahsfal</h5>
+            <br />
+            <h1 style={{ fontWeight: "normal" }}>Create your account</h1>
+            <h5 style={{ fontWeight: "normal" }}>
+               Lorem Ipsum dolor sit amet,consectetur adipiscing elit
+            </h5>
             <GoogleLogin
-            clientId="689688957845-bpk9raj2tcetv9g88r0jd88pktq3p9f0.apps.googleusercontent.com"
-            buttonText="Sign up with Google"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            redirectUri="http://localhost:3000"
+              clientId="689688957845-bpk9raj2tcetv9g88r0jd88pktq3p9f0.apps.googleusercontent.com"
+              buttonText="Sign up with Google"
+              onSuccess={this.responseGoogle}
+              onFailure={this.responseGoogle}
+              redirectUri="http://localhost:3000"
             />
             <FacebookLogin
               appId="848013662672907"
@@ -44,20 +70,32 @@ class SignupForm extends React.Component{
               cssClass="fb"
               buttonText="Sign up with Facebook"
               icon="fa fa-facebook-f"
-              callback={responseFacebook}
+              callback={this.responseFacebook}
             />
-            <br/>
+            <br />
             <hr
-            style={{width: "40%",display: "inline-block",verticalAlign: "middle",}}></hr>{" "}
+              style={{
+                width: "40%",
+                display: "inline-block",
+                verticalAlign: "middle",
+              }}
+            ></hr>{" "}
             <span style={{ color: "grey" }}>or</span>{" "}
-            <hr style={{width: "40%",display: "inline-block",verticalAlign: "middle",}}></hr>
-            <Form onSubmit={(values) => this.handleSubmit(values)}>
+            <hr
+              style={{
+                width: "40%",
+                display: "inline-block",
+                verticalAlign: "middle",
+              }}
+            ></hr>
+            <Form onSubmit={this.handleSubmit}>
               <Row className="form-group">
                 <Col md={12}>
                   <input
                     type="text"
                     placeholder="First Name"
                     className="y"
+                    name="firstname"
                   ></input>
                 </Col>
               </Row>
@@ -67,12 +105,20 @@ class SignupForm extends React.Component{
                     type="text"
                     placeholder="Last Name"
                     className="y"
+                    name="lastname"
                   ></input>
                 </Col>
               </Row>
               <Row className="form-group">
                 <Col md={12}>
-                  <input type="text" placeholder="Email" className="y"></input>
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    className="y"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleEmailChange}
+                  ></input>
                 </Col>
               </Row>
               <Row className="form-group">
@@ -81,6 +127,9 @@ class SignupForm extends React.Component{
                     type="password"
                     placeholder="Password"
                     className="y"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handlePasswordChange}
                   ></input>
                 </Col>
               </Row>
@@ -103,6 +152,7 @@ class SignupForm extends React.Component{
                     type="submit"
                     color="primary"
                     style={{ width: "85%" }}
+                    onClick={this.handleSubmit}
                   >
                     SIGN UP
                   </Button>
@@ -112,7 +162,7 @@ class SignupForm extends React.Component{
           </Col>
         </Row>
       </Container>
-    )
+    );
   }
 }
 
